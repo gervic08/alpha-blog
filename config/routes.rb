84fe553fb48fc469094d239c532bac18d7e5e7fa
gervic08 <germanvicentin@gmail.com>
2022-04-21 11:35:32 -0300

    Articles created also Articles Show feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :articles, only: %i[show]
   root 'pages#home'
-  get 'about', 'pages#about'
+  get 'about', to: 'pages#about'
 end
