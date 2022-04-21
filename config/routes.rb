2bae7f64ac123d5293f38d3becabe99b4e75d993
gervic08 <germanvicentin@gmail.com>
2022-04-21 11:57:44 -0300

    Article Index feature added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :articles, only: %i[show]
+  resources :articles, only: %i[index show new]
   root 'pages#home'
   get 'about', to: 'pages#about'
 end
