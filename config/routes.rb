7ef6877923ad505620e2521659149ba2f37a5919
gervic08 <germanvicentin@gmail.com>
2022-04-21 12:36:31 -0300

    Articles New and Create feature added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :articles, only: %i[index show new]
+  resources :articles, only: %i[index show new create]
   root 'pages#home'
   get 'about', to: 'pages#about'
 end
