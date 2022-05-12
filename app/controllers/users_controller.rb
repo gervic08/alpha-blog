198140a7c4142837d30f69629ac405b9d0a3068e
gervic08 <germanvicentin@gmail.com>
2022-05-12 17:17:33 -0300

    Signup users feature created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alphablog. You´re successfully Signed Up!"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
