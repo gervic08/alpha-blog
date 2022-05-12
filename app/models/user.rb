5a9913836730b8996c7832df998087e1389c0db7
gervic08 <germanvicentin@gmail.com>
2022-05-12 16:20:36 -0300

    Password digest added to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,17 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  email      :string
-#  username   :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  email           :string
+#  password_digest :string
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
+
+  has_secure_password
 
   has_many :articles
 
@@ -25,4 +28,5 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false },
                     length: { maximum: 120 },
                     format: { with: VALID_REGEX_EMAIL }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,8 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_12_175743) do
-
+ActiveRecord::Schema.define(version: 2022_05_12_191533) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,6 +26,6 @@ ActiveRecord::Schema.define(version: 2022_05_12_175743) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
-
 end
