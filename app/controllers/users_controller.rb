10f3889d9ffc44b244542a76fdf0194e296241ad
gervic08 <germanvicentin@gmail.com>
2022-05-13 15:06:39 -0300

    Index users feature added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show update edit]
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user
     @articles = @user.articles
