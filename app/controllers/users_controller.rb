14802f5ee1d79d822251ded82fad0a7077772fed
gervic08 <germanvicentin@gmail.com>
2022-05-13 12:18:43 -0300

    Edit users feature added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[update edit]
+
   def new
     @user = User.new
   end
@@ -15,9 +17,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your user profile was successfully updated."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
-                       length: { minimum: 3, maximum: 20 }
+                       length: { minimum: 3, maximum: 25 }
 
   VALID_REGEX_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
