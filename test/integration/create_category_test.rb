083263dcdd8ba6c0de4d7aff57d00709e5c8275d
gervic08 <germanvicentin@gmail.com>
2022-05-21 16:39:22 -0300

    Category New Form added thorugh TDD

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -3,7 +3,7 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
-  test "get new catgory form and create a category" do
+  test "get new category form and create a category" do
     get "/categories/new"
     assert_response :success
     assert_difference "Category.count", 1 do
@@ -13,5 +13,16 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_response :success
     assert_match "Sports", response.body
+  end
+
+  test "should not create an invalid category" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference "Category.count" do
+      post categories_path, params: { category: { name: " " } }
+    end
+    assert_match "errors", response.body
+    assert_select "div.alert"
+    assert_select "h4.alert-heading"
   end
 end
