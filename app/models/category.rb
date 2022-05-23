90d598cf03b5386b70fa8c9c4c8008e965a17f36
gervic08 <germanvicentin@gmail.com>
2022-05-23 11:56:51 -0300

    Articles-categories association through join table added

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,6 +10,9 @@
 #  updated_at :datetime         not null
 #
 class Category < ApplicationRecord
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+
   validates :name, presence: true,
                    uniqueness: { case_sensitive: false },
                    length: { minimum: 3, maximum: 20}
